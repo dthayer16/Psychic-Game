e6b7af26a025b0e1b2e40e590e602220208fa8d3
David Thayer <dthayer16@gmail.com>
2019-07-17 19:52:01 -0700

    formatting html and adding javascript.

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+var wins = 0;
+
+var losses = 0;
+
+var guessesLeft = 9;
+
+var winsText = document.getElementById("winText");
+var lossesText = document.getElementById("lossText");
+var guessesLeftText = document.getElementById("guessesText");
+var letterSoFarText = document.getElementById("lettersSoFar");
+
+var lettersGuessed = [];
+
+function generateAlphabet() {
+    var alphabet = [];
+    for (var i = 97; i < 123; i++) {
+        alphabet.push(String.fromCharCode(i));
+    }
+    return alphabet;
+};
+
+document.onkeyup = function (e) {
+
+    var userGuess = e.key.toLocaleLowerCase();
+
+    var computerGuess = generateAlphabet(Math.floor(Math.random() * generateAlphabet.length));
+
+    var keysPressed = [];
+
+    if (keysPressed.indexOf(e.key) === -1) {
+        keysPressed.push(e.key);
+
+        if (userGuess === computerGuess) {
+            wins++;
+        } else {
+            losses++;
+            guessesLeft--;
+        }
+
+        winsText.textContent = "wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessesLeftText.textContent = "Guesses Left " + guessesLeft;
+        letterSoFarText.textContent = "You have guessed " + keysPressed;
+    }
+};
